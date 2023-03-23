c0f5849d15c9d6c5c8020f08d065aadb55ddca34
Luis Hong <luishong50@gmail.com>
2023-03-22 20:33:05 -0500

    solution to rossler problem; time step needed to be a value in a particular range

--- sketch.js
+++ sketch.js
@@ -6,15 +6,15 @@ let sigma = 10;
 let rho = 28;
 let beta = 8 / 3;
 
-let a = 0.2;
-let b = 0.2;
-let c = 14;
+let a = 1/5;
+let b = 1/5;
+let c = 5.7;
 let points = [];
 
 function setup() {
     createCanvas(innerWidth, innerHeight, WEBGL);
     // strokeWeight(2)
-    line(0,0,0,0,1,0)
+    // line(0,0,0,0,1,0)
     colorMode(HSB);
 }
 let angle = 0;
@@ -22,8 +22,8 @@ function draw() {
     orbitControl()
     background("black");
 
-    dt = 0.1;
     //lorenz
+    // dt = 0.01;
     // let dx = sigma * (y - x) * dt;
     // let dy = (x * (rho - z) - y) * dt;
     // let dz = (x * y - beta * z) * dt;
@@ -32,13 +32,14 @@ function draw() {
     // z = z + dz;
 
     //rossler
+    dt = 0.05;
     let dx = (-y - z) * dt;
     let dy = (x + a * y) * dt;
     let dz = (b + z * (x - c)) * dt;
     x = x + dx;
     y = y + dy;
     z = z + dz;
-    console.log(x,y,z)
+    // console.log(x,y,z)
 
     // points.push(createVector(x, y, z));
     points.push(new p5.Vector(x, y, z));
@@ -50,6 +51,7 @@ function draw() {
     rotateY(angle);
     rotateX(PI / 2);
     scale(8);
+
     stroke(255);
     noFill();
 
