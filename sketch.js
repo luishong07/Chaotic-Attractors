306083322296f9de00e01b0afd70689637523d0c
Luis Hong <luishong50@gmail.com>
2023-03-23 23:24:10 -0500

    added equations for thomas attractor; low resolution due to step or scale

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let x = 0.124;
+let x = 0.001;
 let y = 0;
 let z = 0;
 
@@ -11,6 +11,8 @@ let b = 1/5;
 let c = 5.7;
 let points = [];
 let cell
+
+let bee = 0.208186
 function setup() {
     createCanvas(innerWidth, innerHeight, WEBGL);
     // strokeWeight(2)
@@ -23,6 +25,13 @@ function draw() {
     orbitControl()
     background("black");
 
+    //thomas
+    // dt = 0.0001
+    // let dx = sin(y) - bee*x
+    // let dy = sin(z) - bee*y
+    // let dz = sin(x) - bee*z
+
+
     //lorenz
     // dt = 0.01;
     // let dx = sigma * (y - x) * dt;
@@ -33,15 +42,16 @@ function draw() {
     // z = z + dz;
 
     //rossler
-    dt = 0.05;
-    let dx = (-y - z) * dt;
-    let dy = (x + a * y) * dt;
-    let dz = (b + z * (x - c)) * dt;
+    // dt = 0.05;
+    // let dx = (-y - z) * dt;
+    // let dy = (x + a * y) * dt;
+    // let dz = (b + z * (x - c)) * dt;
+
     x = x + dx;
     y = y + dy;
     z = z + dz;
     // console.log(x,y,z)
-    cell.show(x,y,z)
+    // cell.show(x,y,z)
     // points.push(createVector(x, y, z));
     points.push(new p5.Vector(x, y, z));
     translate(0, 0, 0);
@@ -51,7 +61,7 @@ function draw() {
     // angle += 0.005;
     rotateY(angle);
     // rotateX(PI / 2);
-    scale(8);
+    scale(15);
 
     stroke(255);
     noFill();
