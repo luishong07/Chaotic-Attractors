f4a78ac41a8a22a1c02f6ee25379ec7ce1ba0c0b
Luis Hong <luishong50@gmail.com>
2023-03-26 00:04:15 -0500

    rossler attractor breaks as numbers grow too large or small even though it does work in the older version of one cell at the time even with same time step

--- sketch.js
+++ sketch.js
@@ -23,30 +23,34 @@ function setup() {
     // strokeWeight(2)
     // line(0,0,0,0,1,0)
     colorMode(HSB);
-    c1 = new Cell(0.001, 0, 0, "red");
-    c2 = new Cell(0.002, 0, 0, "blue");
-    c3 = new Cell(0.003,0,0,"green")
+    c1 = new Cell(0, 0, 0.1, "red");
+    c2 = new Cell(0, 0, 0.02, "blue");
+    c3 = new Cell(0, 0, 0.01, "green");
     cells.push(c1);
     cells.push(c2);
-    cells.push(c3)
+    cells.push(c3);
 }
 let angle = 0;
 function draw() {
     orbitControl();
     background("black");
-
+    // dt = 0.0000001
     //thomas
-    // dt = 0.0001
+    // dt = 0.0001;
     // let dx = sin(y) - bee*x
     // let dy = sin(z) - bee*y
     // let dz = sin(x) - bee*z
 
-    dt = 0.01;
+    dt = 0.05;
     for (let c of cells) {
-        // console.log(c.x,c.y,c.z)
-        let dx = sigma * (c.y - c.x) * dt;
-        let dy = (c.x * (rho - c.z) - c.y) * dt;
-        let dz = (c.x * c.y - beta * c.z) * dt;
+        console.log(c.x,c.y,c.z)
+        let dx = (-c.y - c.z) * dt;
+        let dy = (c.x + a * c.y) * dt;
+        let dz = (b + c.z * (c.x - c)) * dt;
+
+        // let dx = sigma * (c.y - c.x) * dt;
+        // let dy = (c.x * (rho - c.z) - c.y) * dt;
+        // let dz = (c.x * c.y - beta * c.z) * dt;
         let newX = c.x + dx;
         let newY = c.y + dy;
         let newZ = c.z + dz;
@@ -54,13 +58,13 @@ function draw() {
         c.show(newX, newY, newZ);
     }
     //lorenz
-    let dx = sigma * (y - x) * dt;
-    let dy = (x * (rho - z) - y) * dt;
-    let dz = (x * y - beta * z) * dt;
+    // let dx = sigma * (y - x) * dt;
+    // let dy = (x * (rho - z) - y) * dt;
+    // let dz = (x * y - beta * z) * dt;
 
-    let dx2 = sigma * (y2 - x2) * dt;
-    let dy2 = (x2 * (rho - z2) - y2) * dt;
-    let dz2 = (x2 * y2 - beta * z2) * dt;
+    // let dx2 = sigma * (y2 - x2) * dt;
+    // let dy2 = (x2 * (rho - z2) - y2) * dt;
+    // let dz2 = (x2 * y2 - beta * z2) * dt;
 
     //rossler
     // dt = 0.05;
@@ -68,17 +72,15 @@ function draw() {
     // let dy = (x + a * y) * dt;
     // let dz = (b + z * (x - c)) * dt;
 
-    x2 = x2 + dx2;
-    y2 = y2 + dy2;
-    z2 = z2 + dz2;
+    // x2 = x2 + dx2;
+    // y2 = y2 + dy2;
+    // z2 = z2 + dz2;
 
-    x = x + dx;
-    y = y + dy;
-    z = z + dz;
+    // x = x + dx 
     // console.log(x,y,z)
-    // c1.show(x, y, z);
-    // c2.show(x2, y2, z2)
-    // points.push(createVector(x, y, z));
+    // c2.show(x, y, z);
+    // c3.show(x2, y2, z2)
+    points.push(createVector(x, y, z));
     points.push(new p5.Vector(x, y, z));
 
     translate(0, 0, 0);
