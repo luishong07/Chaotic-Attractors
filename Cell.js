29f1482fc8de1e7206c890180e5e13aa6ad3786e
Luis Hong <luishong50@gmail.com>
2023-04-04 19:59:49 -0500

    finally recreated aizawa attractor; problems: carefully define differential equatios and time step needed to be a bit smaller

--- Cell.js
+++ Cell.js
@@ -7,28 +7,27 @@ class Cell {
         this.color = color
     }
 
-    getInitialValues() {
-        return [this.x, this.y, this.z];
-    }
+    // getInitialValues() {
+    //     return [this.x, this.y, this.z];
+    // }
     show(x, y, z) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.path.push(createVector(this.x, this.y, this.z));
-        if (this.path.length > 1500) {
+        if (this.path.length > 3000) {
             this.path.shift();
         }
         stroke(255, 100);
         strokeWeight(10);
-        point(x * 20, y * 20, z * 20);
+        point(x * 30, y * 30, z * 30);
         // console.log(this.color)
         beginShape()
         for (let i = 0; i < this.path.length; i++) {
             stroke(this.color);
             strokeWeight(1);
-            vertex(this.path[i].x * 20, this.path[i].y * 20, this.path[i].z * 20);
+            vertex(this.path[i].x * 30, this.path[i].y * 30, this.path[i].z * 30);
         }
-        // line(0,0,0,x*15, y*15, z*15)
         endShape()
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,15 +1,23 @@
-
-let x = 0.001;
-let y = 0;
-let z = 0;
-
+// const sigma = 10;
+// const rho = 28;
+// const beta = 8 / 3;
 
 // let a = 1 / 5;
 // let b = 1 / 5;
 // let C = 5.7;
-let a = 0.2
-let b = 0.01
-let C = -0.4
+
+// let a = 0.2;
+// let b = 0.01;
+// let C = -0.4;
+
+//aizawa constants
+const a = 0.95
+const b = 0.7
+const c = 0.6
+const d = 3.5
+const e = 0.25
+const f = 0.1
+
 
 let points = [];
 let c1;
@@ -18,10 +26,8 @@ let cells = [];
 let bee = 0.208186;
 function setup() {
     createCanvas(innerWidth, innerHeight, WEBGL);
-    // strokeWeight(2)
-    // line(0,0,0,0,1,0)
     colorMode(HSB);
-    c1 = new Cell(0.1, 0.1, 0.1, "red");
+    c1 = new Cell(0.1, 0, 0, "red");
     c2 = new Cell(0.1, 0, 0.2, "blue");
     c3 = new Cell(0, 0.1, 0.3, "green");
     cells.push(c1);
@@ -30,7 +36,6 @@ function setup() {
 }
 let angle = 0;
 function draw() {
-    // frameRate(20)
     orbitControl();
     background("black");
 
@@ -38,23 +43,30 @@ function draw() {
     // let dx = (a*x + y*z)*dt
     // let dy = (b*x + C*y + x*z)*dt
     // let dz = (-z -x*y)*dt
+
     //thomas
     // dt = 0.0001;
     // let dx = sin(y) - bee*x
     // let dy = sin(z) - bee*y
     // let dz = sin(x) - bee*z
 
-    dt = 0.1;
-    for (let c of cells) {
-        let dx = (a*c.x + c.y*c.z)*dt
-        let dy = (b*c.x + C*c.y - c.x*c.z)*dt
-        let dz = (-c.z -c.x*c.y)*dt
+    dt = 0.01;
+    for (let p of cells) {
 
-        let newX = c.x + dx;
-        let newY = c.y + dy;
-        let newZ = c.z + dz;
-        // console.log(newX, newY, newZ)
-        c.show(newX, newY, newZ);
+
+        // let dx = sigma * (p.y - p.x) * dt;
+        // let dy = (p.x * (rho - p.z) - p.y) * dt;
+        // let dz = (p.x * p.y - beta * p.z) * dt;
+
+        let dx = ((p.z-b)*p.x - d*p.y)*dt
+        let dy = ((d*p.x) + p.y*(p.z - b))*dt
+        let dz = (c + (a*p.z) -((p.z*p.z*p.z)/3) - (((p.x*p.x)+(p.y*p.y))*(1+(e*p.z))) +(f*p.z*p.x*p.x*p.x))*dt
+
+        let newX = p.x + dx;
+        let newY = p.y + dy;
+        let newZ = p.z + dz;
+        console.log(newX, newY, newZ)
+        p.show(newX, newY, newZ);
     }
     //lorenz
     // let dx = sigma * (y - x) * dt;
@@ -67,17 +79,6 @@ function draw() {
     // let dy = (x + a * y) * dt;
     // let dz = (b + z * (x - c)) * dt;
 
-    // x = x + dx;
-    // y = y + dy;
-    // z = z + dz;
-
-    // x = x + dx
-    // console.log(x,y,z)
-    // c1.show(x, y, z);
-    // c3.show(x2, y2, z2)
-    points.push(createVector(x, y, z));
-    points.push(new p5.Vector(x, y, z));
-
     translate(0, 0, 0);
     if (points.length > 500) {
         points.shift();
@@ -86,21 +87,8 @@ function draw() {
     // rotateY(angle);
     // rotateX(PI / 2);
     // console.log(angle)
-    scale(20);
+    scale(30);
     stroke(255);
     noFill();
     let hu = 0;
-    // beginShape();
-
-    // for (let v of points) {
-    //     stroke(hu, 255, 255);
-    //     point(v.x, v.y, v.z);
-
-    //     hu += 1;
-    //     if (hu > 255) {
-    //         hu = 0;
-    //     }
-    // }
-
-    // endShape();
 }
