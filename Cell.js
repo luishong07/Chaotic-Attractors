c069be3220013f1ffc782c9f1af75d0139f8942f
Luis Hong <luishong50@gmail.com>
2023-04-06 21:39:16 -0500

    implemented equations for halvorsen and rabinovich

--- Cell.js
+++ Cell.js
@@ -1,11 +1,12 @@
 class Cell {
-    constructor(x, y, z, color) {
+    constructor(x, y, z, color,scl) {
         this.x = x;
         this.y = y;
         this.z = z;
         this.path = [];
         this.color = color
         this.angle = 0
+        this.scl = scl
     }
 
     // getInitialValues() {
@@ -16,19 +17,21 @@ class Cell {
         this.y = y;
         this.z = z;
         this.path.push(createVector(this.x, this.y, this.z));
-        if (this.path.length > 1500) {
+        if (this.path.length > 3000) {
             this.path.shift();
         }
         rotateY(angle)
         stroke(255, 100);
         strokeWeight(10);
-        point(x * 1, y * 1, z * 1);
+        point(x *this.scl, y *this.scl, z *this.scl);
         // console.log(this.color)
+       
         beginShape()
-        for (let i = 0; i < this.path.length; i++) {
+        for (let i = 1; i < this.path.length; i++) {
             stroke(this.color);
             strokeWeight(1);
-            vertex(this.path[i].x * 1, this.path[i].y * 1, this.path[i].z * 1);
+            vertex(this.path[i].x *this.scl, this.path[i].y *this.scl, this.path[i].z *this.scl);
+
         }
         endShape()
     }
--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-
 //lorenz constants
 // const sigma = 10;
 // const rho = 28;
@@ -26,7 +25,6 @@
 // const f = 4.83;
 // const g = 4.66;
 
-
 //dadras constants
 // const a = 3
 // const b = 2.7
@@ -35,21 +33,29 @@
 // const e = 9
 
 //chen constants
-const alpha = 5
-const beta = -10
-const delta = -0.38
+// const alpha = 5;
+// const beta = -10;
+// const delta = -0.38;
+
+//halvorsen constans
+// const a = 1.89
+
+//rabonivich-fabrikant
+let alpha = 0.14;
+let sigma = 0.10;
 
 let points = [];
 let c1;
 let c2;
 let cells = [];
 let bee = 0.208186;
+let scl = 20;
 function setup() {
     createCanvas(innerWidth, innerHeight, WEBGL);
     colorMode(HSB);
-    c1 = new Cell(0.1, 0.1, 0.2, "red");
-    c2 = new Cell(0.1, 0, 0.2, "blue");
-    c3 = new Cell(0.2, 0.1, 0.3, "green");
+    c1 = new Cell(0.1, 0.1, 0.2, "red", scl);
+    c2 = new Cell(0.1, 0, 0.2, "blue", scl);
+    c3 = new Cell(0.2, 0.1, 0.3, "green", scl);
     cells.push(c1);
     cells.push(c2);
     cells.push(c3);
@@ -81,19 +87,15 @@ function draw() {
     // let dy = (-1 * p.y + p.x * p.y - b * p.x * p.z + g) * dt;
     // let dz = (-1 * p.z + b * p.x * p.y + p.x * p.z) * dt;
 
-
     //dadras
     // let dx =( p.y - (a*p.x) + (b*p.y*p.z))*dt
     // let dy = ((c*p.y) - (p.x*p.z) + p.z)*dt
     // let dz = ((d*p.x*p.y) - (e*p.z))*dt
-    dt = 0.0006;
+    dt = 0.02;
     for (let p of cells) {
-
-        let dx =(alpha*p.x -p.y*p.z)*dt
-        let dy = (beta*p.y + p.x*p.z)*dt
-        let dz = (delta*p.z +((p.x*p.y)/3))*dt
-
-
+        let dx = (p.y * (p.z - 1 + p.x ** 2) + sigma * p.x)*dt;
+        let dy = (p.x * (3 * p.z + 1 - p.x ** 2) + sigma * p.y)*dt;
+        let dz = (-2 * p.z * (alpha + p.x * p.y))*dt;
 
         let newX = p.x + dx;
         let newY = p.y + dy;
@@ -101,6 +103,19 @@ function draw() {
         // console.log(newX, newY, newZ);
         p.show(newX, newY, newZ);
     }
+
+    //halvorsen
+    // dt = 0.01;
+    // let dx = (-1*a*p.x - 4*p.y - 4*p.z -p.y**2)*dt
+    // let dy = (-1*a*p.y - 4*p.z - 4*p.x -p.z**2)*dt
+    // let dz = (-1*a*p.z - 4*p.x - 4*p.y -p.x**2)*dt
+
+    //chen
+    // dt = 0.00125;
+    // let dx = (alpha * p.x - p.y * p.z) * dt;
+    // let dy = (beta * p.y + p.x * p.z) * dt;
+    // let dz = (delta * p.z + (p.x * p.y) / 3) * dt;
+
     //lorenz
     // let dx = sigma * (p.y - p.x) * dt;
     // let dy = (p.x * (rho - p.z) - p.y) * dt;
@@ -113,9 +128,9 @@ function draw() {
     // let dz = (b + z * (x - c)) * dt;
 
     translate(0, 0, 0);
-    if (points.length > 500) {
-        points.shift();
-    }
+    // if (points.length > 500) {
+    //     points.shift();
+    // }
     // angle += 0.005;
     // rotateY(angle);
     // rotateX(PI / 2);
@@ -123,5 +138,5 @@ function draw() {
     // scale(10);
     stroke(255);
     noFill();
-    let hu = 0;
+    // let hu = 0;
 }
